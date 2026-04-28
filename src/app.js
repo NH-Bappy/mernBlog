7c971b9a90b39252da95fb58bd2d01a1d707d3e8
NH-Bappy <naimulbappy2207@gmail.com>
2026-04-28 22:54:26 +0600

    working on mearn blog

--- src/app.js
+++ src/app.js
@@ -1,15 +1,16 @@
 const express = require('express');
+const app = express();
 require('dotenv').config();
 const userController = require('./controller/user.controller');
 const categoryController = require('./controller/categoryController');
-const app = express();
+
 
 //use of middleware
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-
+//registration
 
 app.post("/registration",userController.registration);
 app.post("/login",userController.login);
---
+++ src/models/blog.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const {Schema} = new mongoose;
+
+
+
+const blogSchema = new Schema({
+    userId: {
+        type: mongoose.Schema.ObjectId,
+        ref: "users",
+    },
+    blogTile: {
+        type:String,
+        required: true,
+        trim: true,
+    },
+    blogDescription:{
+        type:String,
+        required:true,
+        trim: true,
+    }
+})
