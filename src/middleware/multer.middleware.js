44c19e6db292b6585fb559c5fcf93ac2a815c433
NH-Bappy <naimulbappy2207@gmail.com>
2026-05-01 23:55:29 +0600

    working on multer

--- src/middleware/multer.middleware.js
+++ src/middleware/multer.middleware.js
@@ -1,15 +1,14 @@
 const multer = require("multer");
-
+const path = require("path");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '../../public/temp')
+    const uploadPath = path.join(__dirname, "../../public/temp");
   },
   filename: function (req, file, cb) {
     // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
     // cb(null, file.fieldname + '-' + uniqueSuffix)
     console.log(file)
-    return
     cb(null, file.originalname)
   }
 })
