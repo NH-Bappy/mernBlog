5667b5bfab3fc5282beb99894c98d92ec05ce85d
NH-Bappy <naimulbappy2207@gmail.com>
2026-05-04 22:08:10 +0600

    working on update

--- src/controller/blog.controller.js
+++ src/controller/blog.controller.js
@@ -38,7 +38,7 @@ exports.allBlog = async (req, res) => {
             })
         }
         res.status(201).json({
-            msg: "successfully found all the blog" ,
+            msg: "successfully found all the blog",
             allBlog,
         })
     } catch (error) {
@@ -52,9 +52,9 @@ exports.allBlog = async (req, res) => {
 
 exports.singleBlog = async (req, res) => {
     try {
-        const {id} = req.params;
+        const { id } = req.params;
         const singleBlog = await blogModel.findById(id)
-        if(!singleBlog){
+        if (!singleBlog) {
             res.status(401).json({
                 msg: "there is no blog found",
             })
@@ -64,7 +64,7 @@ exports.singleBlog = async (req, res) => {
             singleBlog,
         })
     } catch (error) {
-        console.log("error from single Blog" ,error)
+        console.log("error from single Blog", error)
         res.status(500).json({
             msg: "error from single Blog",
             error,
@@ -73,6 +73,21 @@ exports.singleBlog = async (req, res) => {
 }
 
 
-exports.updateBlog = async(req ,res) => {
-    
+exports.updateBlog = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const { blogTitle, blogDescription } = req.body;
+        const image = req.file;
+        
+
+
+
+
+    } catch (error) {
+        console.log("error from update blog controller", error);
+        res.status(500).json({
+            msg: "error from update blog controller",
+            error,
+        });
+    }
 }
